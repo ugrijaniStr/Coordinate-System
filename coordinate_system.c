4dc54f4fedd94786bb7fbcea33a37fdcf5d41f1e
Krkan <74649190+Krkanoid@users.noreply.github.com>
2021-10-16 23:02:22 +0200

    Update coordinate_system.c

--- coordinate_system.c
+++ coordinate_system.c
@@ -2,33 +2,51 @@
 
 int main() {
     int x,y,z;
-    int i,j,l;
+    int j,i,l;
+    int m,n,b;
     int s;
-    for(x = 0; x <=0; ++x) {
-        for(y = 0; y < 6; y++) {
-            for(l = 0; l < 23; l++) {
+
+    for(x = 0; x <= 0; x++) {
+        for(y = 10; y > 0; y--) {
+            for(z = 0; z <= 40; z++) {
                 printf(" ");
             }
-            printf("|");
-            printf("\n");
-        }
-        for(z = 0; z < 56; z++) {
-            printf("_");
-            if(z == 55) {
-                printf("\n");
+            if(y == 10) {
+                printf("%d %s ",y,"| \n");
             } else {
-                printf("");
+                printf("%d %s ",y,"| \n");
             }
         }
-        for(i = 0; i <= 0; i++) {
-            for(j = 0; j < 6; ++j) {
-                for(s = 0; s < 23; s++) {
-                    printf(" ");
+        for(j = 0; j <= 0; j++) {
+            for(i = 10; i > -1; --i) {
+                if(i == 0) {
+                    printf("%d |  ",i);
+                } else {
+                    printf("%d   ",i);
                 }
-                printf("|");
-                printf("\n");
+            }
+            for(l = -1; l > -11; l--) {
+                printf("%d   ",l);
+            }
+        }
+        printf("\n");
+        for(m = 0; m < 100; m++) {
+            printf("-");
+        }
+        for(s = 0; s <= 10; s++) {
+            printf(" ");
+        }
+        printf("\n");
+        printf(" ");
+        for(n = -1; n > -11; n--) {
+            for(b = 0; b <= 39; b++) {
+                printf(" ");
+            }
+            if(n == -10) {
+                printf("%d%s",n,"|\n");
+            } else {
+                printf("%d %s ",n,"| \n");
             }
         }
     }
-    return 0;
 }
